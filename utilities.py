21a777e85f82ff5914a43b6d3f6b8bb4e733669a
NoSenseKid <93103852+NoSenseKid@users.noreply.github.com>
2026-02-05 15:38:41 +0800

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,10 @@
+import math
+
+def convertCtoF(x):
+    return (x*1.8)+32
+
+def areaCircle(r):
+    return math.pi*(r^2)
+    
+def areaTriangle(x1,y1,x2,y2,x3,y3):
+    return 0.5*abs((x1*(y2+y3))+(x2*(y1+y3))+(x3*(y1+y2)))
